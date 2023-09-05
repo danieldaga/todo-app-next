cc7ed8aa93ee495757e30f89dd4ccc04b8ed1c07
danieldaga <daniel@arhrestauracion.com>
2023-09-05 22:08:17 +0200

    create task component

--- todo-app/src/app/page.tsx
+++ todo-app/src/app/page.tsx
@@ -1,15 +1,19 @@
+import { getAllTodos } from "../../api";
 import AddTask from "../app/components/AddTask";
 import TodoList from "../app/components/TodoList";
 
 
-export default function Home() {
+export default async function Home() {
+  const tasks = await getAllTodos()
+  console.log(tasks);
+  
   return (
     <main className="max-w-4xl mx-auto mt-4">
       <div className="text-center my-5 flex flex-col gap-4">
         <h1 className="text-2xl font-bold">To Do List App</h1>
         <AddTask />
       </div>
-      <TodoList />
+      <TodoList tasks={tasks} />
     </main>
   )
 }
