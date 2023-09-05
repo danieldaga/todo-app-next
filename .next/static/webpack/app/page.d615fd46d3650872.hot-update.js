63b9ed48506785a8e35e85b68026093cff6bfb79
danieldaga <daniel@arhrestauracion.com>
2023-09-05 19:30:19 +0200

    files

---
+++ .next/static/webpack/app/page.d615fd46d3650872.hot-update.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("app/page",{
+
+/***/ "(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fdanielgarcia%2FDesktop%2Fprojects23%2FNextjs%2Ftodo-app-next%2Fpublic%2Fnext.svg&server=false!":
+/*!***********************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fdanielgarcia%2FDesktop%2Fprojects23%2FNextjs%2Ftodo-app-next%2Fpublic%2Fnext.svg&server=false! ***!
+  \***********************************************************************************************************************************************************************************************************/
+/***/ (function(__unused_webpack_module, __unused_webpack_exports, __webpack_require__) {
+
+eval(__webpack_require__.ts("Promise.resolve(/*! import() eager */).then(__webpack_require__.bind(__webpack_require__, /*! ./public/next.svg */ \"(app-pages-browser)/./public/next.svg\"))//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL25vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvYnVpbGQvd2VicGFjay9sb2FkZXJzL25leHQtZmxpZ2h0LWNsaWVudC1lbnRyeS1sb2FkZXIuanM/bW9kdWxlcz0lMkZVc2VycyUyRmRhbmllbGdhcmNpYSUyRkRlc2t0b3AlMkZwcm9qZWN0czIzJTJGTmV4dGpzJTJGdG9kby1hcHAtbmV4dCUyRnB1YmxpYyUyRm5leHQuc3ZnJnNlcnZlcj1mYWxzZSEiLCJtYXBwaW5ncyI6IkFBQUEiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLz83NGRmIl0sInNvdXJjZXNDb250ZW50IjpbImltcG9ydCgvKiB3ZWJwYWNrTW9kZTogXCJlYWdlclwiICovIFwiL1VzZXJzL2RhbmllbGdhcmNpYS9EZXNrdG9wL3Byb2plY3RzMjMvTmV4dGpzL3RvZG8tYXBwLW5leHQvcHVibGljL25leHQuc3ZnXCIpIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///(app-pages-browser)/./node_modules/next/dist/build/webpack/loaders/next-flight-client-entry-loader.js?modules=%2FUsers%2Fdanielgarcia%2FDesktop%2Fprojects23%2FNextjs%2Ftodo-app-next%2Fpublic%2Fnext.svg&server=false!\n"));
+
+/***/ }),
+
+/***/ "(app-pages-browser)/./public/next.svg":
+/*!*************************!*\
+  !*** ./public/next.svg ***!
+  \*************************/
+/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval(__webpack_require__.ts("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = ({\"src\":\"/_next/static/media/next.3baf90a6.svg\",\"height\":80,\"width\":394,\"blurWidth\":0,\"blurHeight\":0});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiKGFwcC1wYWdlcy1icm93c2VyKS8uL3B1YmxpYy9uZXh0LnN2ZyIsIm1hcHBpbmdzIjoiO0FBQUEsK0RBQWUsQ0FBQyxtR0FBbUciLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9fTl9FLy4vcHVibGljL25leHQuc3ZnPzMzNjIiXSwic291cmNlc0NvbnRlbnQiOlsiZXhwb3J0IGRlZmF1bHQge1wic3JjXCI6XCIvX25leHQvc3RhdGljL21lZGlhL25leHQuM2JhZjkwYTYuc3ZnXCIsXCJoZWlnaHRcIjo4MCxcIndpZHRoXCI6Mzk0LFwiYmx1cldpZHRoXCI6MCxcImJsdXJIZWlnaHRcIjowfTsiXSwibmFtZXMiOltdLCJzb3VyY2VSb290IjoiIn0=\n//# sourceURL=webpack-internal:///(app-pages-browser)/./public/next.svg\n"));
+
+/***/ })
+
+});
