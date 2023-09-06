2ccf4e5554bf66aa4aca9cb87d7121d738ad9112
danieldaga <daniel@arhrestauracion.com>
2023-09-06 12:49:16 +0200

    button edit done

--- todo-app/src/app/components/Task.tsx
+++ todo-app/src/app/components/Task.tsx
@@ -1,17 +1,51 @@
+"use client";
+
 import { ITask } from "../../../types/tasks"
 import { FiEdit, FiTrash2 } from "react-icons/fi"
+import { useState } from "react"
+import Modal from "./Modal"
 
 interface TaskProps {
     task: ITask
 }
 
 const Task: React.FC<TaskProps> = ({task}) =>{
+const [openModalEdit, setOpenModalEdit] = useState<boolean>(false)
+const [openModalDeleted, setOpenModalDeleted] = useState<boolean>(false)
+const [taskToEdit, setTaskToEdit] = useState<string>(task.text)
+const handleSubmitEditTodo: FormEventHandler<HTMLFormElement> = 
+async (e) => {
+    e.preventDefault()
+    await addTodo({
+        id: uuidv4(),
+        text: newTaskValue
+    })
+    setNewTaskValue("")
+    setModalOpen(false)
+    router.refresh();
+}
+
 return(
     <tr key={task.id}>
-        <td>{task.text}</td>
+        <td className="w-full">{task.text}</td>
         <td className="flex gap-5">
-            <FiEdit size={25} />
-            <FiTrash2 size={25} />
+            <FiEdit onClick={() => setOpenModalEdit(true)} cursor='pointer' className="text-blue-400" size={25} />
+                <Modal modalOpen={openModalEdit} setModalOpen={setOpenModalEdit}>
+                    <form onSubmit={handleSubmitEditTodo}>
+                        <h3 className="font-bold text-lg">Edit task</h3>
+                        <div className="modal-action">
+                            <input
+                                // value={taskToEdit}
+                                // onChange={(e) = setTaskToEdit (e.target.value)}
+                                type="text" 
+                                placeholder="Type here" 
+                                className="input input-bordered w-full" 
+                            />
+                            <button type="submit" className="btn">Submit</button>
+                        </div>
+                    </form>
+                </Modal>
+            <FiTrash2 cursor='pointer' className="text-red-400" size={25} />
         </td>
     </tr>
 )
