057a17521e4d19eabdaed27ea4977684a4dae96a
danieldaga <daniel@arhrestauracion.com>
2023-09-06 12:09:44 +0200

    button edit and delete

--- todo-app/src/app/components/Task.tsx
+++ todo-app/src/app/components/Task.tsx
@@ -1,4 +1,5 @@
 import { ITask } from "../../../types/tasks"
+import { FiEdit, FiTrash2 } from "react-icons/fi"
 
 interface TaskProps {
     task: ITask
@@ -8,7 +9,10 @@ const Task: React.FC<TaskProps> = ({task}) =>{
 return(
     <tr key={task.id}>
         <td>{task.text}</td>
-        <td></td>
+        <td className="flex gap-5">
+            <FiEdit size={25} />
+            <FiTrash2 size={25} />
+        </td>
     </tr>
 )
 
