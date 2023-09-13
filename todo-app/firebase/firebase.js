f35b1bc5f09b7a22a393d3d5410d1b0a758277af
danieldaga <daniel@arhrestauracion.com>
2023-09-13 18:56:13 +0200

    firebase install

---
+++ todo-app/firebase/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDPg0AoSEAJIxdlHuhKwmK1xZDGuVR2268",
+  authDomain: "todoapp-537e3.firebaseapp.com",
+  projectId: "todoapp-537e3",
+  storageBucket: "todoapp-537e3.appspot.com",
+  messagingSenderId: "1096332528283",
+  appId: "1:1096332528283:web:5bb03ca6ca63c280cdd43e",
+  measurementId: "G-TQB4C5Q3F6"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
